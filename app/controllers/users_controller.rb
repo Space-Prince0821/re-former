fd54169a8ec046d2a056dfe7ae122c760b647ef0
Prince <Prince.chowdhury.246@my.csun.edu>
2023-10-04 21:43:23 -0400

    Adding edit user method, route and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,22 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    # @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    
   end
 
   private
